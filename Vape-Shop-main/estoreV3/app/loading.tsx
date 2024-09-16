92f0166cc40aa42a9c43e631d8fe09215e53ce23
IVC <ievgenchuchukalo.com>
2024-09-16 10:39:25 -0500

    first commit

---
+++ Vape-Shop-main/estoreV3/app/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  // can add any UI inside Loading, including a Skeleton.
+  return (
+    <>
+      <h1>Loading...</h1>
+    </>
+  )
+}
---
+++ Vape-Shop-main/estoreV3/drizzle.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "drizzle-kit"
+import * as dotenv from "dotenv"
+
+dotenv.config({
+  path: ".env.local",
+})
+
+export default defineConfig({
+  dialect: "postgresql",
+  schema: "./server/schema.ts",
+  out: "./server/migrations",
+  dbCredentials: {
+    url: process.env.POSTGRES_URL!,
+  },
+})
+
+/// npx drizzle-kit studio
